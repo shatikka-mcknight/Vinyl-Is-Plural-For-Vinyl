a1fa71adbc4fa10873e36f5aed55edc3c34f63dc
Richard Yvarra <richard.yvarra@gmail.com>
2021-05-16 12:30:02 -0400

    userScriptadditions

--- public/js/userScript.js
+++ public/js/userScript.js
@@ -1,1 +1,25 @@
 //script for buy/sell query
+
+const albumConditionButton = async function(event){
+    event.preventDefault();
+
+    const ele = document.getElementsByName('condition').value;
+
+    const response = await fetch(,{
+        method: 'POST',
+        body: JSON.stringify({
+            condition: ele.value
+            
+        }),
+        headers:{ 'Content-Type': 'application/json'},
+    });
+    document.location.reload();
+        
+    }
+    document.querySelector('#conditionButton')
+    .addEventListener('click', albumConditionButton)
+
+
+
+    
+
