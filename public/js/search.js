75f85d695e8b56c32fb33a096247d1bc2cb35b93
Richard Yvarra <richard.yvarra@gmail.com>
2021-05-16 12:58:41 -0400

    changes to search js

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,27 @@
 const searchButtonHandler = async function(event){
     event.preventDefault()
 
+    let _data ={
+      artist_id:this.artist_id,
+      title: this.title,
+      format: this.format,
+      year: this.year,
+      catno: this.catno,
+      country:this.country,
+      condition: this.condition,
+    
 
- fetch(`https://api.discogs.com/database/search?type=artist&q=&token=cHZUIbGIvpsKYHSyNhoDoyyXafHfIMXumACktuLd`)
+
+
+    }
+
+
+ fetch(`https://api.discogs.com/database/search?type=artist&q=&token=cHZUIbGIvpsKYHSyNhoDoyyXafHfIMXumACktuLd`,{
+   method: 'POST',
+   body: JSON.stringify(_data),
+   headers: {
+     'Content-type': 'application/json'}
+ })
 
   
   .then(response => response.json())
